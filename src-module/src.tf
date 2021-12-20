66f6f9a8de48615bb603320b8398bba77df04fc3
12dop <88845080+12dop@users.noreply.github.com>
2021-12-20 16:26:25 +0100

    Update src.tf

--- src-module/src.tf
+++ src-module/src.tf
@@ -1,5 +1,6 @@
 module "src" {
-source = "../modules/src"      
+source = "../modules/src" 
+  codestar_connector_credentials = "arn:aws:codestar-connections:eu-west-2:160064204913:connection/c0c73919-188b-47bf-b03c-96cdbcebb562"
 }
   variable "codestar_connector_credentials" {
     type = string
