2a2b98bb7fe39b919e0f619f5bc618a3dcf682f3
12dop <88845080+12dop@users.noreply.github.com>
2021-12-20 16:39:14 +0100

    Update S3bucket.tf

--- modules/bucket/S3bucket.tf
+++ modules/bucket/S3bucket.tf
@@ -4,8 +4,3 @@ resource "aws_s3_bucket" "codepipeline_artifacts" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket" "damitest-aws-cicd-pipeline" {
-  bucket = "damitest-aws-cicd-pipeline"
-  acl    = "private"
-force_destroy = true
-} 
