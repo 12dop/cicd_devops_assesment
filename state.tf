5475969dd1e99d01b7b852362f47327b0df07e77
12dop <88845080+12dop@users.noreply.github.com>
2021-12-20 03:35:36 +0100

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,11 +1,11 @@
-terraform{
-    backend "s3" {
-        bucket = "damitest-aws-cicd-pipeline"
-        encrypt = true
-        key = "terraform.tfstate"
-        region = "eu-west-2"
-    }
-}
+#terraform{
+ #   backend "s3" {
+  #      bucket = "damitest-aws-cicd-pipeline"
+   #     encrypt = true
+    #    key = "terraform.tfstate"
+     #   region = "eu-west-2"
+    #}
+#}
 
 
 
